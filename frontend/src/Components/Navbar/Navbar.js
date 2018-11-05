849421b2bd23789e870b1b13cd17db4ca78425c0
Shawn Stewart <s.stewart0220@gmail.com>
2018-11-05 07:22:07 -0500

    Gradients are in sync. Moved navbar hamburger

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -6,14 +6,14 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="Navbar ets-container">
-        <a href="localhost:3000" className="Navbar-link">
-          <h1 className="Navbar-logo">Every Ting Sweet</h1>
-        </a>
         <span className="Navbar-hamburger">
           <div className="hamburger-bar1" />
           <div className="hamburger-bar2" />
           <div className="hamburger-bar3" />
         </span>
+        <a href="localhost:3000" className="Navbar-link">
+          <h1 className="Navbar-logo">Every Ting Sweet</h1>
+        </a>
       </nav>
     );
   }
