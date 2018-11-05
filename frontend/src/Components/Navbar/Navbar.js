d6374940d30024af6b46d6b837f78d4453aadc38
Shawn Stewart <s.stewart0220@gmail.com>
2018-11-05 13:20:37 -0500

    Hamburger changing on click

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -3,10 +3,28 @@ import React, { Component } from "react";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMobileMenu: false
+    };
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({ showMobileMenu: !this.state.showMobileMenu });
+  };
+
   render() {
     return (
       <nav className="Navbar ets-container">
-        <span className="Navbar-hamburger">
+        <span
+          className={
+            "Navbar-hamburger " +
+            (this.state.showMobileMenu ? "Navbar-hamburger-open" : "")
+          }
+          onClick={this.toggleMobileMenu}
+        >
           <div className="hamburger-bar1" />
           <div className="hamburger-bar2" />
           <div className="hamburger-bar3" />
