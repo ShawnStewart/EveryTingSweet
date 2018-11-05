6b611e7eed9464da1eb678f1441c7da1ef9b931a
Shawn Stewart <s.stewart0220@gmail.com>
2018-11-04 19:49:35 -0500

    Created a basic landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Landing from "./Components/Landing/Landing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <div className="App-body">
+          <Landing />
+        </div>
       </div>
     );
   }
---
+++ frontend/src/Components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Landing.css";
+
+export default function Landing() {
+  return (
+    <div className="Landing">
+      <div className="Landing-CTA">
+        <button className="Landing-CTA-button About">About Us</button>
+        <button className="Landing-CTA-button Order">Order</button>
+      </div>
+      <span className="test">hello</span>
+    </div>
+  );
+}
