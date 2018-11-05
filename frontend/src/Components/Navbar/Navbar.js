7e4962fc0d446eb81aaa8c5d4dcbe9de36751e03
Shawn Stewart <s.stewart0220@gmail.com>
2018-11-05 14:34:47 -0500

    Mobile side menu slides in

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -7,32 +7,48 @@ export default class Navbar extends Component {
     super(props);
 
     this.state = {
+      activeMenuItem: "",
       showMobileMenu: false
     };
   }
 
   toggleMobileMenu = () => {
+    const navbar = document.querySelector(".Navbar");
+    const appbody = document.querySelector(".App-body");
+    const mobilemenu = document.querySelector(".Navbar-mobile-menu");
+    if (!this.state.showMobileMenu) {
+      navbar.style.marginLeft = "250px";
+      appbody.style.marginLeft = "250px";
+      mobilemenu.style.width = "250px";
+    } else {
+      navbar.style.marginLeft = "0";
+      appbody.style.marginLeft = "0";
+      mobilemenu.style.width = "0";
+    }
     this.setState({ showMobileMenu: !this.state.showMobileMenu });
   };
 
   render() {
     return (
-      <nav className="Navbar ets-container">
-        <span
-          className={
-            "Navbar-hamburger " +
-            (this.state.showMobileMenu ? "Navbar-hamburger-open" : "")
-          }
-          onClick={this.toggleMobileMenu}
-        >
-          <div className="hamburger-bar1" />
-          <div className="hamburger-bar2" />
-          <div className="hamburger-bar3" />
-        </span>
-        <a href="localhost:3000" className="Navbar-link">
-          <h1 className="Navbar-logo">Every Ting Sweet</h1>
-        </a>
-      </nav>
+      <div>
+        <div className="Navbar-mobile-menu">Hey</div>
+        <nav className="Navbar ets-container">
+          <span
+            className={
+              "Navbar-hamburger " +
+              (this.state.showMobileMenu ? "Navbar-hamburger-open" : "")
+            }
+            onClick={this.toggleMobileMenu}
+          >
+            <div className="hamburger-bar1" />
+            <div className="hamburger-bar2" />
+            <div className="hamburger-bar3" />
+          </span>
+          <a href="localhost:3000" className="Navbar-link">
+            <h1 className="Navbar-logo">Every Ting Sweet</h1>
+          </a>
+        </nav>
+      </div>
     );
   }
 }
